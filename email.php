37fd03423bdfe6964ff876fba468fd6f674c31a8
Nishanth0303 <109363213+Nishanth0303@users.noreply.github.com>
2024-11-26 13:07:31 +0530

    Update email.php

--- email.php
+++ email.php
@@ -3,10 +3,10 @@
 <?php
 
 
-$to = "r.nishanth2004@gmail.com"; // Replace with the recipient's email address
+$to = "xxxx@gmail.com"; // Replace with the recipient's email address
 $subject = "Test Email"; // Email subject
 $message = "This is a test email sent using PHP."; // Email body
-$headers = "From: rkteams2022@gmail.com"; // Replace with sender's email address
+$headers = "From: 123@gmail.com"; // Replace with sender's email address
 
 if (mail($to, $subject, $message, $headers)) {
     echo "Email sent successfully.";
